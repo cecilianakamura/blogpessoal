45bd5f20690c845916be436ee2ba32ef16c71dab
ceci.n.nakamura@gmail.com <ceci.n.nakamura@gmail.com>
2022-10-13 17:42:05 -0300

    Criando classe UserDetailsImpl na camada Security

---
+++ src/main/java/com/generation/blogpessoal/security/UserDetailsServiceImpl.java
@@ -0,0 +1,7 @@
+package com.generation.blogpessoal.security;
+
+import org.springframework.security.core.userdetails.UserDetailsService;
+
+public class UserDetailsServiceImpl implements UserDetailsService{
+
+}
