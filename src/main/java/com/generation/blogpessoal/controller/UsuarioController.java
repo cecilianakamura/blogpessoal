4fe64eb4659a1d3029496fa65bd43f738cfc79b2
ceci.n.nakamura@gmail.com <ceci.n.nakamura@gmail.com>
2022-10-14 14:09:36 -0300

    Adicionando anotação @PutMapping no método atualizar Usuario

--- src/main/java/com/generation/blogpessoal/controller/UsuarioController.java
+++ src/main/java/com/generation/blogpessoal/controller/UsuarioController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -60,6 +61,7 @@ public class UsuarioController {
 		
 	}
 	
+	@PutMapping("/atualizar")
 	public ResponseEntity<Usuario> putUsuario(@Valid @RequestBody Usuario usuario){
 		return service.atualizarUsuario(usuario)
 				.map(resposta->ResponseEntity.status(HttpStatus.OK).body(resposta))
