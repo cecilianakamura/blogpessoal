37cd4cbae88844734e9e1528df389d63e3c6634f
ceci.n.nakamura@gmail.com <ceci.n.nakamura@gmail.com>
2022-10-14 10:28:45 -0300

    Adicionando métodos cadastrar e atualizar usuário

--- src/main/java/com/generation/blogpessoal/service/UsuarioService.java
+++ src/main/java/com/generation/blogpessoal/service/UsuarioService.java
@@ -1,31 +1,64 @@
 package com.generation.blogpessoal.service;
 
 import java.nio.charset.Charset;
+import java.util.Optional;
 
 import org.apache.tomcat.util.codec.binary.Base64;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
+import com.generation.blogpessoal.model.Usuario;
+import com.generation.blogpessoal.repository.UsuarioRepository;
 
 @Service
 public class UsuarioService {
+
+	@Autowired
+	private UsuarioRepository usuarioRepository;
+
+	public Optional<Usuario> cadastrarUsuario(Usuario usuario) {
+		if (usuarioRepository.findByUsuario(usuario.getUsuario()).isPresent()) // acessei no mySQL e fiz uma comparação
+																				// para ver se já existe
+			throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Usuário já cadastrado", null);
+
+		usuario.setSenha(criptografarSenha(usuario.getSenha()));
+
+		return Optional.of(usuarioRepository.save(usuario));
+	}
+
+	public Optional<Usuario> atualizarUsuario(Usuario usuario) {
+
+		if (usuarioRepository.findById(usuario.getId()).isPresent()) {
+			Optional<Usuario> buscarUsuario = usuarioRepository.findByUsuario(usuario.getUsuario());
+			if (buscarUsuario.isPresent()) {
+				if (buscarUsuario.get().getId() != usuario.getId())
+					throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Usuário já existe!", null);
+			}
+			usuario.setSenha(criptografarSenha(usuario.getSenha()));
+		}
+		
+		throw new ResponseStatusException(HttpStatus.NOT_FOUND,"Usuário não encontrado!",null);
+
+	}
 
 	private String criptografarSenha(String senha) {
 		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
 		String senhaEncoder = encoder.encode(senha);
 		return senhaEncoder;
 	}
-	
+
 	private boolean compararSenhas(String senhaDigitada, String senhaBanco) {
 		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
 		return encoder.matches(senhaDigitada, senhaBanco);
 	}
-	
+
 	private String gerarBasicToken(String email, String password) {
 		String estrutura = email + ":" + password;
 		byte[] estruturaBase64 = Base64.encodeBase64(estrutura.getBytes(Charset.forName("US-ASCII")));
 		return "Basic " + new String(estruturaBase64);
 	}
 
-	
 }
