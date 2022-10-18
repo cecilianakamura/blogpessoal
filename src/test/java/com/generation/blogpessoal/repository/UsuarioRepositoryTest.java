01acb0e1df9a235dee16788291410e77d20ef773
ceci.n.nakamura@gmail.com <ceci.n.nakamura@gmail.com>
2022-10-18 11:26:59 -0300

    Adicionando teste para retorno de Três Usuários que contenham nome Silva

--- src/test/java/com/generation/blogpessoal/repository/UsuarioRepositoryTest.java
+++ src/test/java/com/generation/blogpessoal/repository/UsuarioRepositoryTest.java
@@ -4,7 +4,7 @@ package com.generation.blogpessoal.repository;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-
+import java.util.List;
 import java.util.Optional;
 
 import org.junit.jupiter.api.BeforeAll;
@@ -44,4 +44,17 @@ public class UsuarioRepositoryTest {
 		
 		assertTrue(usuario.get().getUsuario().equals("joao@email.com.br"));
 	}
+	
+	@Test
+	@DisplayName("Retorna 3 usuarios")
+	public void deveRetornarTresUsuarios() {
+		
+		List<Usuario> listaDeUsuarios = usuarioRepository.findAllByNomeContainingIgnoreCase("Silva");
+		assertEquals(3, listaDeUsuarios.size());
+		assertTrue(listaDeUsuarios.get(0).getNome().equals("João da Silva"));
+		assertTrue(listaDeUsuarios.get(1).getNome().equals("Manuela da Silva"));
+		assertTrue(listaDeUsuarios.get(2).getNome().equals("Adriana da Silva"));
+	}
+
+	
 }
